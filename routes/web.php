4fb538b38b046393531d60b819dd5beb5dc042b0
Sovath <sovathyut45@gmail.com>
2026-07-11 16:30:31 +0700

    Add reset password web route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/reset-password', function (Request $request) {
+
+    return response()->json([
+        'token' => $request->token,
+        'email' => $request->email,
+    ]);
+
+});
