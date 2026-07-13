a3183b62a36d29371eca739bf050cb5db0e1c0f7
Sovath <sovathyut45@gmail.com>
2026-07-13 23:57:36 +0700

    Implement forgot password and reset password flow

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -8,13 +8,14 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Carbon\Carbon;
 
 class ResetPasswordController extends Controller
 {
     public function resetPassword(Request $request) {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required|confirmed',
+            'password' => 'required|min:8|confirmed',
             'token' => 'required',
         ]);
 
@@ -33,6 +34,18 @@ class ResetPasswordController extends Controller
                 'success' => false,
                 'error' => 'Invalid token'
             ] , 400);
+        }
+
+        if (Carbon::parse($reset->created_at)->diffInMinutes(now()) > 60) {
+
+            DB::table('password_reset_tokens')
+                ->where('email', $reset->email)
+                ->delete();
+
+            return response()->json([
+                'success' => false,
+                'error' => 'Token expired'
+            ], 400);
         }
 
         $user = User::where(
